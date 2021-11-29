c1b572911eca36b4c8db3c0410016ca2fba38891
Ermahan-Nurgazy <malgazhdar.ermahan@gmail.com>
2021-11-29 11:02:30 +0600

    Create project

---
+++ assets/MainAsset.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace app\assets;
+
+
+use yii\web\AssetBundle;
+
+class MainAsset extends AssetBundle
+{
+
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/main.css',
+    ];
+    public $js = [
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap4\BootstrapAsset',
+    ];
+
+}
---
+++ views/layouts/elma.php
@@ -0,0 +1,27 @@
+<?php
+
+use app\assets\MainAsset;
+
+MainAsset::register($this);
+
+?>
+<?php $this->beginPage() ?>
+<!doctype html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= \yii\helpers\Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<?= $content ?>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
